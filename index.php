66469a035d6c2e3954fbc16e4e4180e1b2d9018e
Gokul Reghu <gokulreghu007@gmail.com>
2019-08-25 09:28:51 -0700

    Link for required API in different lines

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-Welcome
+Welcome<br>
 <a href="https://bankapitrials.herokuapp.com/bank.php/?ifsc=ABHY0065014">First API</a><br>
 <a href="https://bankapitrials.herokuapp.com/api2.php?bank=UCO%20BANK&city=MUMBAI&O=0&L=10">Second API</a>
 

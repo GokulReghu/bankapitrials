a160046f9c14f4adf2447a58773611dea70a4eb7
Gokul Reghu <gokulreghu007@gmail.com>
2019-08-25 08:52:48 -0700

    Heroku db connect

--- api2.php
+++ api2.php
@@ -7,7 +7,7 @@ if($_GET['O'] != NULL) {
 if($_GET['L'] != NULL) {
 	$L=$_GET['L'];
 }
-$db_connection = pg_connect("host=localhost dbname=bank1 user=postgres password=dragondropeone");
+$db_connection = pg_connect("host=ec2-54-221-198-156.compute-1.amazonaws.com dbname=d45a4or7lhq7uu user=qguoalgvcjdlct password=028fb3f0e85ca4b531a7088ff130893275daa5b762ae9c1be4143607d2aab02f");
 $query = "SELECT * FROM bank_branches WHERE bank_name='".$_GET['bank']."' AND city='".$_GET['city']."' OFFSET '".$O."' LIMIT '".$L."'";
 $result = pg_query($db_connection, $query);
 $resultArr = pg_fetch_all($result); 
--- bank.php
+++ bank.php
@@ -1,5 +1,5 @@
 <?php
-$db_connection = pg_connect("host=localhost dbname=bank1 user=postgres password=dragondropeone");
+$db_connection = pg_connect("host=ec2-54-221-198-156.compute-1.amazonaws.com dbname=d45a4or7lhq7uu user=qguoalgvcjdlct password=028fb3f0e85ca4b531a7088ff130893275daa5b762ae9c1be4143607d2aab02f");
 $query = "SELECT * FROM bank_branches WHERE ifsc='".$_GET['ifsc']."'OFFSET 10 LIMIT 10";
 $result = pg_query($db_connection, "SELECT * FROM bank_branches WHERE ifsc='".$_GET['ifsc']."'");
 $resultArr = pg_fetch_assoc($result);
